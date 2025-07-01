0800e978f0d153e4a9fd0444a96563f6a277756d
DvK42 <guillaume.couvidou@live.fr>
2025-07-01 02:29:50 +0200

    Ajout boutons de difficulté + adaptation temps du timer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,56 @@
 <script setup>
-import {computed} from 'vue';
-import {storeToRefs} from 'pinia';
+import { computed } from "vue";
+import { storeToRefs } from "pinia";
 
-import {useMinesweeperStore} from '@/stores/minesweeper';
-import Table from '@/components/minesweeper/Table.vue';
+import { useMinesweeperStore } from "@/stores/minesweeper";
+import Table from "@/components/minesweeper/Table.vue";
 import Timer from "@/components/minesweeper/Timer.vue";
 
 const store = useMinesweeperStore();
-const {minesweeper, isFinished, dimension} = storeToRefs(store);
-const {createMinesweeper, resetMinesweeper, handleFlagMode} = store;
+const { minesweeper, isFinished, dimension, difficulty, duration } = storeToRefs(store);
+const { createMinesweeper, resetMinesweeper, handleFlagMode, setDifficulty, setDuration } = store;
 
 const max = computed(() => {
   return minesweeper.value
-    ? minesweeper.value.dimension * minesweeper.value.dimension - minesweeper.value.nbBombByDifficulty[minesweeper.value.level]
+    ? minesweeper.value.dimension * minesweeper.value.dimension -
+        minesweeper.value.nbBombByDifficulty[minesweeper.value.level]
     : 0;
 });
 
+const difficultyLabels = {
+  1: "Facile",
+  2: "Moyen",
+  3: "Difficile",
+};
+
+const handleDifficultyChange = (level) => {
+  setDifficulty(level);
+  if (minesweeper.value) {
+    createMinesweeper(level);
+  }
+};
 </script>
 
 <template>
   <h1>Démineur</h1>
-  <button v-if="!minesweeper" @click="createMinesweeper(1)">Nouvelle partie</button>
+
+  <div>
+    <button
+      v-for="(label, level) in difficultyLabels"
+      :key="level"
+      :class="{ active: difficulty == level }"
+      @click="handleDifficultyChange(parseInt(level))"
+    >
+      {{ label }}
+    </button>
+  </div>
+
+  <button v-if="!minesweeper" @click="createMinesweeper()">Nouvelle partie</button>
   <button v-if="minesweeper" @click="resetMinesweeper()">Abandonner</button>
 
   <button v-if="minesweeper" @click="handleFlagMode()">Flag</button>
   <p v-if="minesweeper">{{ minesweeper.score }} / {{ max }}</p>
 
-  <Timer
-    :gameStarted="dimension > 0"
-    :gameFinished="isFinished"
-    :duration="3"
-  />
-  <Table/>
+  <Timer :gameStarted="dimension > 0" :gameFinished="isFinished" :duration="duration" />
+  <Table />
 </template>
